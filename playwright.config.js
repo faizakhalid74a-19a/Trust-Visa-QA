506f5a93354eabd56406025cf16f596c5b07e9a3
faizakhalid74a-19a <faizakhalid74a@gmail.com>
2026-05-28 11:19:28 +1200

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   timeout: 30000,
+  reporter: 'html',
   use: {
     headless: true,
     screenshot: 'only-on-failure',
